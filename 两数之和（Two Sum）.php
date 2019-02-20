e05ac5b43836e901f34e95e59a394fe83b0b94a5
范承 <bestfancc@gmail.com>
2019-02-20 12:18:25 +0800

    Update 两数之和（Two Sum）.php

--- 两数之和（Two Sum）.php
+++ 两数之和（Two Sum）.php
@@ -1,9 +1,10 @@
 /**
  * by bestfancc<bestfancc@gmail.com>
- * 时间复杂度为O(n),php数组的底层是用散列表实现的，所以array_search函数的
- *      时间复杂度应该等同散列表取数据的时间复杂度O(1)
+ * 
+ * 时间复杂度为O(n),php数组的底层是用散列表实现的，所以array_search函数的时间复杂度应该等同散列表取数据的时间复杂度O(1)  
  * 空间复杂度为O(1)
  *
+ **/
 class Solution {
     function twoSum($nums, $target) {
         $return = array();
