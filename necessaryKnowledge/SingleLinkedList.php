ce88d946e600b700f0324c1aeccac1142b43e2fa
bestfancc <bestfancc@gmail.com>
2019-03-20 23:15:04 +0800

    单链表

---
+++ necessaryKnowledge/SingleLinkedList.php
@@ -0,0 +1,11 @@
+<?php
+//手打代码学习基础数据结构，来自 https://github.com/bestfancc/algo/blob/master/php/06_linkedlist/SingleLinkedList.php
+
+
+namespace Algo_06;
+
+
+class SingleLinkedList
+{
+
+}
--- necessaryKnowledge/SingleLinkedListNode.php
+++ necessaryKnowledge/SingleLinkedListNode.php
@@ -3,5 +3,13 @@
 
 namespace Algo_06;
 
-
-
+class SingleLinkdListNode
+{
+    public $data;
+    public $next;
+    public function __construct($data = null)
+    {
+        $this->data = $data;
+        $this->next = null;
+    }
+}
